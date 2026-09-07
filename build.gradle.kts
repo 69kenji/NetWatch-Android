ab6b13fe9ddeb7220d54eede2cc303a5d6b96ee0
unknown <geokis04@gmail.com>
2026-09-07 06:23:02 +0300

    Fix Build Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,45 @@ buildscript {
     }
 
     dependencies {
+        components {
+            withModule("org.apache.httpcomponents:httpmime") {
+                allVariants {
+                    withDependencies {
+                        removeAll {
+                            it.group == "org.apache.httpcomponents" && it.name == "httpclient"
+                        }
+                        add("org.apache.httpcomponents:httpclient:4.5.14")
+                    }
+                }
+            }
+            withModule("org.apache.commons:commons-compress") {
+                allVariants {
+                    withDependencies {
+                        removeAll {
+                            it.group == "org.apache.commons" && it.name == "commons-lang3"
+                        }
+                        add("org.apache.commons:commons-lang3:3.18.0")
+                    }
+                }
+            }
+            withModule("com.android.tools:sdk-common") {
+                allVariants {
+                    withDependencies {
+                        removeAll {
+                            it.group == "org.bouncycastle" && it.name in setOf(
+                                "bcpkix-jdk18on",
+                                "bcprov-jdk18on",
+                                "bcutil-jdk18on",
+                            )
+                        }
+                        add("org.bouncycastle:bcpkix-jdk18on:1.84")
+                        add("org.bouncycastle:bcprov-jdk18on:1.84")
+                        add("org.bouncycastle:bcutil-jdk18on:1.84")
+                    }
+                }
+            }
+        }
+
         // Keep AGP's build-only transitive dependencies on patched releases.
         classpath("org.jdom:jdom2:2.0.6.1")
         classpath("org.apache.httpcomponents:httpclient:4.5.14")
